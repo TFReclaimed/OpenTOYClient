1554f5d1f5555587066ed0a9d98f85393ab7aa71
Nuutti <snuutti@protonmail.com>
2026-04-05 19:17:17 +0300

    Save session npsn and token

--- Packages/OpenTOYClient/Runtime/NPA/TOY/ToySession.cs
+++ Packages/OpenTOYClient/Runtime/NPA/TOY/ToySession.cs
@@ -1,9 +1,38 @@
+using UnityEngine;
+
 namespace NPA.TOY
 {
     internal class ToySession
     {
-        public long Npsn { get; set; }
-        public string NpToken { get; set; }
+        public long Npsn
+        {
+            get => _npsn;
+            set
+            {
+                PlayerPrefs.SetInt("ToyNpsn", (int) value);
+                _npsn = value;
+            }
+        }
+
+        public string NpToken
+        {
+            get => _npToken;
+            set
+            {
+                PlayerPrefs.SetString("ToyNpToken", value);
+                _npToken = value;
+            }
+        }
+
         public string ServiceId { get; set; }
+
+        private long _npsn;
+        private string _npToken;
+
+        public ToySession()
+        {
+            Npsn = PlayerPrefs.GetInt("ToyNpsn", 0);
+            NpToken = PlayerPrefs.GetString("ToyNpToken", string.Empty);
+        }
     }
 }
